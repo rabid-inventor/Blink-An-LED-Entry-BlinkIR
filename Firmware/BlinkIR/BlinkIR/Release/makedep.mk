69e7bebf55f966a1cfaca8bb466cfb0d2cd23cfd
Gee Bartlett <geeus81@gmail.com>
2018-09-09 22:20:15 +0100

    Finish working POC for revision 1 of BlinkIR

--- Firmware/BlinkIR/BlinkIR/Release/makedep.mk
+++ Firmware/BlinkIR/BlinkIR/Release/makedep.mk
@@ -4,13 +4,3 @@
 
 main.c
 
-uart_format.c
-
-uart_print.c
-
-uart_recv.c
-
-uart_send.c
-
-utility.c
-
--- Firmware/BlinkIR/BlinkIR/main.c
+++ Firmware/BlinkIR/BlinkIR/main.c
@@ -7,71 +7,81 @@
 //#define  F_CPU	800000
 
 #define BLINK_DELAY 500
+
+#define BLINK_PIN 2
+#define IR_SEND_PIN 0
+#define IR_RECEIVE_PIN 1
+#define RAND_MAX  0xFF
+
 #include <stdlib.h>
 #include <avr/io.h>
-#include <avr/delay.h>
-
+#include <util/delay.h>
 #include <avr/interrupt.h>
+#include <avr/power.h>
 
-//#include "softuart.h"
-//#include <math.h>
+void io_setup(){
+	
+	
+	
+	DDRB = ( (1 << BLINK_PIN ) | ( 1 << IR_SEND_PIN) | ( 0 << IR_RECEIVE_PIN ) ); // set inputs and outputs for pins 
+	PORTB |= _BV(IR_RECEIVE_PIN);  // Set pullup for ir recieve pin 
+}
+
+char pool_ir_input(){
+	
+	char ir_received = PINB & (1 << IR_RECEIVE_PIN) ;
+	
+	if (ir_received  == 0){
+		
+		return 1;
+	}
+	
+		else{
+			
+			return 0;
+			} 
+}
 
 
+void blink(){
+		PORTB |= _BV(BLINK_PIN);
+		PORTB |= _BV(IR_SEND_PIN);
+		
+		_delay_ms(BLINK_DELAY);
+		
+		PORTB &= ~(_BV(BLINK_PIN));
+		PORTB &= ~(_BV(IR_SEND_PIN));
+		
+		_delay_ms(BLINK_DELAY);
+	
+}
 
 int main(void)
 {
 	
-	   // Interrupt-Section:
-	   
-	   EICRA = 0x03;    // Low-Level generates an interrupt
-	   EIMSK = 0x01;    // External Interrupt Request 0 Enable
-	   PCICR = 0x01;    // Pin Change Interrupt Control Register set to enable PCINT Interrupt
-	   PCMSK = 0x04;    // Pin Change Mask Register set to enable PCINT0
-
-
-	DDRB = 0xFF;
-	sei();
+	io_setup();
 	
-	PORTB &= ~(_BV(2));
-	//uartInit();
-	char uart_input;
-	 char count  = 0x00;
-	 
-	 int cycle_delay ;
-	 cycle_delay = rand();
-    /* Replace with your application code */
+	int delay;
+	PORTB &= ~(_BV(1));
     while (1) 
 	
     {
-		/*
-		uartSend(count);
+	    delay = rand();
 		
-		if (uartAvail()){
-			uart_input = uartRecv();
-			PORTB |= ( 1 << PINB0);
-			if (uart_input == count){
-				PORTB |= ( 1 << PINB0) ;
-			}
+		blink();
+		
+		while( delay > 0  ){
 			
+			if(pool_ir_input() == 1 )
+			{
+				blink() ;
+				_delay_ms(2000);
+				delay = rand() ;
+			}
+			_delay_us(100);
+			delay--;
 		}
-		count++;
-		*/
-		
-		PORTB |= _BV(0);
-		_delay_ms(BLINK_DELAY);	
-		PORTB &= ~(_BV(0));
-		
-		
-		_delay_ms(BLINK_DELAY);	
-		
-		_delay_ms(cycle_delay);
 		
     }
 }
 
-ISR(PCINT0_vect) {
- cli();
- //PORTB |= _BV(2);
- sei();
- return;
-}
